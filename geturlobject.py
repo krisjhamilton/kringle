0aecea79ebba3e7a20bada426a01533d148ebdb8
krisjhamilton <krisjhamilton@gmail.com>
2018-12-22 16:34:02 +1300

    Updates to RID

--- geturlobject.py
+++ geturlobject.py
@@ -9,10 +9,11 @@ status = False
 f = open( "file.txt", "r" ).read()
 lines = f.split("\n")
 lineno = ""
+rid = 'd802d8b3-be77-4ab8-94ea-933a41d6f98e'
 while not status:
     for line in lines:
         lineno = line
-        u = "https://doorpasscode.kringlecastle.com/checkpass.php?i="+line
+        u = "https://doorpasscode.kringlecastle.com/checkpass.php?i="+line+"&resourceId="+rid
         url = urllib2.urlopen(u).read()
         obj = json.loads(url.decode('utf-8'))
         success = obj["success"]
